c492af9234d32a77f887c5d9d648c82c81020504
cccanshviradiya <ccc.anshviradiya@gmail.com>
2026-02-05 11:55:14 +0530

    requirements update

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ A Streamlit application for comprehensive exploratory data analysis
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+
 import plotly.express as px
 import plotly.graph_objects as go
 from io import BytesIO
